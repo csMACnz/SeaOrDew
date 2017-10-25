bf082c1b89550519876c9e505b54043885fb02a4
Mark Clearwater <csmacnz@csmac.co.nz>
2017-10-25 08:33:35 +0100

    convert to coverage scripts

---
+++ Coverage.ps1
@@ -0,0 +1,4 @@
+
+dotnet test -c Release .\src\csMACnz.SeaOrDew.Tests\csMACnz.SeaOrDew.Tests.csproj
+    
+dotnet test -c Release .\src\csMACnz.SeaOrDew.AspNetCoreTests\csMACnz.SeaOrDew.AspNetCoreTests.csproj
